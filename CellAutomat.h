ce0dab7661f5b59800ebd4ac9e488f5f5fe1d6fb
Ozanis <37501284+ozanis@users.noreply.github.com>
2019-07-11 23:59:41 +0300

    Color depth

--- CellAutomat.h
+++ CellAutomat.h
@@ -13,12 +13,122 @@ using std::uniform_real_distribution;
 
 
 typedef struct {
-    bool ** grid;
+    int ** grid;
     size_t width;
     size_t height;
 }Patern;
 
+int ** init(unsigned height, unsigned width){
+    int ** newMap = new int*[height*sizeof(int)];
+    for(unsigned i = 0; i < height; i++){
+        newMap[i] = new int[width]{0};
+    }
+    return newMap;
+}
 
+
+void display(Patern * map){
+    for(unsigned i = 0; i < map->height; i++){
+        cout << endl;
+        for(unsigned j = 0; j < map->width; j++){
+            if(map->grid[i][j]) cout << " ";
+            else cout << "#";
+        }
+    } cout << endl;
+}
+
+
+class CellAutomat{
+public:
+    CellAutomat(float chance, int deathLimit, int birthLimit, unsigned h, unsigned w);
+    ~CellAutomat();
+    int ** grid = nullptr;
+    unsigned height;
+    unsigned width;
+    int countAliveNeighbours(unsigned x, unsigned y);
+    void update(unsigned steps);
+    Patern * synthesis();
+
+private:
+    int ** runSimulation();
+    float stayAliveChance;
+    int deathLimit;
+    int birthLimit;
+};
+
+
+CellAutomat :: CellAutomat(float chance, int death, int birth, unsigned h, unsigned w){
+    this->stayAliveChance = chance;
+    this->deathLimit = death;
+    this->birthLimit = birth;
+    this->height = h;
+    this->width = w;
+    this->grid = init(h, w);
+
+    default_random_engine generator;
+    uniform_real_distribution<float> distribution(0, 1);
+    for(unsigned i = 1; i < this->height; i++ ){
+        for(unsigned j = 1; j < this->width; j++ ){
+            this->grid[i][j] = distribution(generator) < this->stayAliveChance;
+        }
+    }
+}
+
+
+CellAutomat :: ~CellAutomat(){
+    delete(grid);
+}
+
+
+int CellAutomat :: countAliveNeighbours(unsigned x, unsigned y){
+    int count = 0;
+    for(int i=-1; i<2; i++){
+        for(int j=-1; j<2; j++){
+            int neighbour_i = x+i;
+            int neighbour_j = y+j;
+            if(i == 0 && j == 0 && i == this->height - 1 && j == this->width) continue;
+            if(neighbour_i < 0 || neighbour_j < 0 || neighbour_i >= this->height || neighbour_i >= this->width) ++count;
+            else if(this->grid[neighbour_i][neighbour_j]) ++count;
+        }
+    }
+    return count;
+}
+
+
+int ** CellAutomat :: runSimulation() {
+    int ** newMap = init(this->height, this->width);
+    for (int i = 1; i < this->height-1; i++) {
+        for (int j = 1; j < this->width-1; j++) {
+            int nbs = this->countAliveNeighbours(i, j);
+            if (this->grid[i][j]) {
+                if(nbs >= deathLimit) newMap[i][j] = nbs;
+                else newMap[i][j] = 0;
+            }
+            else {
+                if(nbs > birthLimit) newMap[i][j] = nbs;
+            }
+        } cout << endl;
+    }
+    return newMap;
+}
+
+
+void CellAutomat :: update(unsigned steps){
+   for(unsigned i = 0; i < steps; i++){
+       this->grid = runSimulation();
+   }
+}
+
+
+Patern * CellAutomat :: synthesis(){
+    auto * map = new Patern[sizeof(Patern)];
+    map->grid = this->grid;
+    map->height = this->height;
+    map->width = this->width;
+    return map;
+}
+
+/*
 bool ** init(unsigned height, unsigned width){
     bool ** newMap = new bool*[height*sizeof(bool)];
     for(unsigned i = 0; i < height; i++){
@@ -127,5 +237,5 @@ Patern * CellAutomat :: synthesis(){
     map->width = this->width;
     return map;
 }
-
+*/
 #endif //AUTOMATADUNGEONE_CELLAUTOMAT_H
--- ImageProcessor.h
+++ ImageProcessor.h
@@ -9,6 +9,104 @@ using std::cerr;
 using std::cout;
 
 
+static int brownPalette[8][3] = {
+        {22, 184, 135},
+        {156, 82, 17},
+        {144, 72, 10},
+        {102, 51, 0},
+        {204, 102, 0},
+        {173, 156, 0},
+        {127, 73, 26},
+        {107, 54, 8}
+};
+
+
+static int greenPalette[8][3] = {
+        {0, 204, 0},
+        {76, 153, 0},
+        {51, 102, 0},
+        {0, 102, 51},
+        {0, 153, 0},
+        {102, 51, 0},
+        {127, 73, 26},
+        {28, 208, 58}
+};
+
+
+static int bluePalette[8][3]{
+        {102, 178, 255},
+        {51, 153, 255},
+        {0, 128, 255},
+        {0, 102, 204},
+        {0, 76, 153},
+        {0, 51, 102},
+        {51, 51, 255},
+        {0, 255, 255}
+};
+
+
+class ColorPallete{
+public:
+    template <typename P, unsigned pos, unsigned RGB>
+    ColorPallete(gdImagePtr image, P (&clr)[pos][RGB]);
+    ~ColorPallete() = default;
+    int color[8] = {0};
+};
+
+template <typename P, unsigned pos, unsigned RGB>
+ColorPallete :: ColorPallete(gdImagePtr image, P (&clr)[pos][RGB]) {
+    for(unsigned i = 0; i < 8; i++){
+        color[i] = gdImageColorAllocate(image, );
+    }
+}
+
+
+
+
+
+class Png{
+public:
+    Png(Patern * map, char const * path);
+    ~Png() = default;
+    void genPng(unsigned scale);
+    friend int CellAutomat :: countAliveNeighbours(unsigned x, unsigned y);
+
+    char const * path = nullptr;
+    Patern * raw = nullptr;
+    FILE * pngOut = nullptr;
+};
+
+
+Png :: Png(Patern * map, char const * path){
+    this->raw = map;
+    this->path = path;
+    cout << raw->width << endl << raw->height << endl;
+}
+
+
+void Png :: genPng(unsigned scale){
+   gdImagePtr image = gdImageCreate(scale * this->raw->height, scale * this->raw->width);
+   if(image == nullptr) {
+       cerr << "unable to open image";
+       _exit(0);
+   }
+   int black = gdImageColorAllocate(image, 0, 0, 0);
+   int brown = gdImageColorAllocate(image, 222, 184, 135);
+   for(unsigned i =0; i < this->raw->height; i ++){
+        for(unsigned j = 0; j < this->raw->width; j ++){
+                if(this->raw->grid[i][j]) gdImageRectangle(image, i*scale, j*scale, i*scale+scale, j*scale+scale, black);
+                else gdImageRectangle(image, i, j, i*scale, j*scale, brown[countAliveNeighbours(i, j)]);
+        }
+    }
+    this->pngOut = fopen(this->path, "wb");
+    gdImagePng(image, this->pngOut);
+    fclose(this->pngOut);
+    gdImageDestroy(image);
+}
+
+
+
+/*
 class Png{
 public:
     Png(Patern * map, char const * path);
@@ -47,6 +145,6 @@ void Png :: genPng(unsigned scale){
     fclose(this->pngOut);
     gdImageDestroy(image);
 }
-
+*/
 
 #endif //AUTOMATADUNGEONE_IMAGEPROCESSOR_H
